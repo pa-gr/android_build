pa-gr/android_build

commit e6bfa1a5d25ca344fb51230d9a6121b341b6b06a

[master] Enable madvising of odex, vdex and art files
Enable madvsing odex, vdex and art files to MADV_WILLNEED by setting the
size limits to UINT32_MAX.

Test: presubmit
Bug: 178853586
Change-Id: I7647a4be214dcd43341f216aba0a7791f877e379

target/product/runtime_libart.mk
@@ -98,3 +98,10 @@ PRODUCT_USES_DEFAULT_ART_CONFIG := true
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.iorapd.enable=false
 
+# Enable Madvising of the whole art, odex and vdex files to MADV_WILLNEED.
+# The size specified here is the size limit of how much of the file
+# (in bytes) is madvised. Setting it to UINT_MAX.
+PRODUCT_SYSTEM_PROPERTIES += \
+    dalvik.vm.madvise.vdexfile.size=4294967295 \
+    dalvik.vm.madvise.odexfile.size=4294967295 \
+    dalvik.vm.madvise.artfile.size=4294967295

commit 57b272f8a36ffa203fbef2258e688dd642a48e6f

runtime_libart: Fixup dalvik madvise properties
PRODUCT_SYSTEM_PROPERTIES doesn't exist yet (only exists in master)
so use PRODUCT_SYSTEM_DEFAULT_PROPERTIES like other props instead

Fixes: e6bfa1a5d25ca344fb51230d9a6121b341b6b06a ("Enable madvising of odex, vdex and art files")
Change-Id: Ice9718f4fe37d906e1428932507a5d10fa21c25d

target/product/runtime_libart.mk
@@ -103,7 +103,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # (in bytes) is madvised.
 # We madvise the whole .art file to MADV_WILLNEED with UINT_MAX limit.
 # For odex and vdex files, we limit madvising to 100MB.
-PRODUCT_SYSTEM_PROPERTIES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.madvise.vdexfile.size=104857600 \
     dalvik.vm.madvise.odexfile.size=104857600 \
     dalvik.vm.madvise.artfile.size=4294967295